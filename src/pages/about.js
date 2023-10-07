4229c31fa38776c6002d31c595929c80f50001d5
obayomi96 <obayomimartins96@gmail.com>
2023-10-07 10:11:27 +0100

    rm netlify forms

--- src/pages/about.js
+++ src/pages/about.js
@@ -89,35 +89,6 @@ const AboutPage = () => {
           </a>
           , I'll add a contact form soon!{" "}
         </p>
-        <div>
-          <form name="contact" netlify netlify-honeypot="bot-field" hidden>
-            <input type="text" name="name" />
-            <input type="email" name="email" />
-            <textarea name="message"></textarea>
-          </form>
-          <form name="contact" method="POST" netlify>
-            <input type="hidden" name="form-name" value="contact" />
-            <p>
-              <label>
-                Name <input type="text" name="name" />
-              </label>
-            </p>
-            <p>
-              <label>
-                Email <input type="email" name="email" />
-              </label>
-            </p>
-
-            <p>
-              <label>
-                Message: <textarea rows={2} name="message"></textarea>
-              </label>
-            </p>
-            <p>
-              <button type="submit">Send</button>
-            </p>
-          </form>
-        </div>
       </div>
     </Layout>
   );
